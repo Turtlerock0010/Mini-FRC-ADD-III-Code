398c3e6158c15da26041211f929cf0441d5634fa
Turtlerock0010 <danielrprincipe@gmail.com>
2024-12-10 20:59:56 -0500

    Version 7

--- main.c
+++ main.c
@@ -15,66 +15,64 @@ NoU_Motor pickUpMotor(5);
 NoU_Motor shooterMotor(6);
 
 //servo motors init
-NoU_Motor aimServo(1);
+NoU_Servo aimServo(1);
 
 //drive train init
 NoU_Drivetrain drivetrain(&frontleftMotor, &frontrightMotor, &backleftMotor, &backrightMotor);
 
-int aimDegree = 0; // don't know the standard position servo is gonna be
+int aimDegree = 180; // don't know the standard position servo is gonna be
 int rotationSpeed = 2; //Should be fast enough
 
 void setup() {
-    // I like to mew
-    Serial.begin(9600); // the bauder rate gotta be 9600 or 115200
-    PestoLink.begin("Anthony Demetri Demarcus III"); // black name to not miss a shot
+  // I like to mew
+  Serial.begin(115200); // the bauder rate gotta be 9600 or 115200
+  PestoLink.begin("Anthony Demetri Demarcus III"); //
 }
-
 void loop() {
-    //--Joystick Controls--
-    float rotation = 0;
-    float throttle = 0;
+  //--Joystick Controls--
+  float rotation = 0;
+  float throttle = 0;
 
-    // Set the throttle of the robot based on what key is pressed
-    // wah wah set a value to negative
-    rotation = -1 * PestoLink.getAxis(0);
-    throttle =  1 * PestoLink.getAxis(1);
+  // Set the throttle of the robot based on what key is pressed
+  // wah wah set a value to negative
+  rotation = -1 * PestoLink.getAxis(0);
+  throttle =  1 * PestoLink.getAxis(1);
 
-    // Make the robot drive
-    drivetrain.arcadeDrive(throttle, rotation);
-    //--End of Joystick Controls--
+  // Make the robot drive
+  drivetrain.arcadeDrive(throttle, rotation);
+  //--End of Joystick Controls--
 
-    //--Motor Functions--
-    //Pickup Motor
-    if (PestoLink.buttonHeld(2)) {
-        pickUpMotor.set(1);
-    }
-    else {
-        pickUpMotor.set(0);
-    }
+  //--Motor Functions--
+  //Pickup Motor
+  if (PestoLink.buttonHeld(2)) {
+    pickUpMotor.set(-1);
+  }
+  else {
+    pickUpMotor.set(0);
+  }
 
-    //Shooter Motor
-    if (PestoLink.buttonHeld(3)) {
-        shooterMotor.set(-1);
-    }
-    else {
-        shooterMotor.set(0);
-    }
-    //--End of Motor Functions
+  //Shooter Motor
+  if (PestoLink.buttonHeld(3)) {
+    shooterMotor.set(-1);
+  }
+  else {
+    shooterMotor.set(0);
+  }
+  //--End of Motor Functions
 
-    //--Servo Functions--
+  //--Servo Functions--
 
-    //Aim motor
-    if (PestoLink.buttonHeld(0)) { // there is no parameters to stop robot from kms-ing itself but i don't care
-        aimDegree -= rotationSpeed;
-        aimServo.write(aimDegree);
-        //Join the Emperor's Coven today!
-    }
-    if (PestoLink.buttonHeld(1)) {
-        aimDegree += rotationSpeed;
-        aimServo.write(aimDegree);
-        //Damn I wish I could play Helldivers 2 rn
-    }
-
-    //--End of Servo Functions--
-    PestoLink.update();
+  //Aim motor
+  if (PestoLink.buttonHeld(0)) { // there is no parameters to stop robot from kms-ing itself but i don't care
+    aimServo.write(aimDegree) - 1;
+    aimDegree--;
+    //Join the Emperor's Coven today!
+  }
+  if (PestoLink.buttonHeld(1)) {
+    aimServo.write(aimDegree) + 1;
+    aimDegree++;
+    //Damn I wish I could play Helldivers 2 rn
+  }
+  //--End of Servo Functions--
+  PestoLink.update();
 }
